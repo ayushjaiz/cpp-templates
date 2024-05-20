a7605683fbae84d2f0acfd67a3d923cfc6512fac
Ayush Jaiswal <ayush45aj@gmail.com>
2024-05-20 11:59:50 +0530

    Update Greatest_element_with_frequency.cpp

--- Greatest_element_with_frequency.cpp
+++ Greatest_element_with_frequency.cpp
@@ -1,45 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long int
 #define pii pair<int, int>
-#define ff first
-#define ss second
-
 int mod = 1e9 + 7;
-int add(int a, int b) { return (a + b + mod) % mod; }
 
-// segment tree (without lazy updates)
 class segment_tree
 {
-    // *endpoints:
-    //
-    // seg.query(left, right);
-    // seg.updatePoint(point, value);
-
     vector<pii> tree;
-    vector<int> a;
     int n;
-    pii identity = {0, 1};
-    pii purpose(pii a, pii b)
+    pii identity;
+
+    pii purpose(pii &a, pii &b)
     {
-        if (a.ff > b.ff)
+        if (a.first == 0 and b.first == 0)
             return a;
-        else if (a.ff < b.ff)
-            return b;
+
+        if (a.first == b.first)
+            return {a.first, (a.second + b.second) % mod};
+        else if (a.first > b.first)
+            return a;
         else
-        {
-            if (a.ff == 0)
-                return {0, 1};
-            else
-                return {a.ff, add(a.ss, b.ss)};
-        }
+            return b;
     }
     void build_util(int start, int end, int index)
     {
         if (start == end)
         {
-            tree[index] = {a[start], 1};
+            tree[index] = identity;
             return;
         }
         int mid = (start + end) / 2;
@@ -49,6 +36,7 @@ class segment_tree
 
         tree[index] = purpose(tree[2 * index], tree[2 * index + 1]);
     }
+
     pii query_util(int qs, int qe, int index, int start, int end)
     {
         if (qs <= start and end <= qe)
@@ -82,14 +70,14 @@ class segment_tree
     }
 
 public:
-    segment_tree(vector<int> &a)
+    segment_tree(int n)
     {
         this->n = n;
-        this->a = a;
+        identity = {0, 1};
 
         tree.resize(4 * n + 1, identity);
 
-        build_util(0, a.size() - 1, 1);
+        build_util(0, n - 1, 1);
     }
     pii query(int left, int right)
     {
@@ -101,45 +89,43 @@ public:
     }
 };
 
+bool comp(pair<int, int> &a, pair<int, int> &b)
+{
+    if (a.first != b.first)
+        return a.first < b.first;
+
+    return a.second > b.second;
+}
+
 int32_t main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int n;
     cin >> n;
 
-    vector<int> a(n);
-    vector<pii> vp(n);
+    vector<int> nums(n);
     for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-        vp[i] = {a[i], i};
-    }
+        cin >> nums[i];
 
-    vector<int> a_copy(n);
+    vector<pair<int, int>> nums_map(n);
+    for (int i = 0; i < n; i++)
+        nums_map[i] = {nums[i], i};
+
+    sort(nums_map.begin(), nums_map.end(), comp);
 
     vector<int> pos(n);
-    sort(vp.begin(), vp.end());
     for (int i = 0; i < n; i++)
-        pos[vp[i].ss] = i, a_copy[i] = vp[i].ff;
+        pos[nums_map[i].second] = i;
 
-    vector<int> count(n + 1, 0);
-
-    segment_tree seg(count);
+    segment_tree seg(n + 1);
 
     for (int i = 0; i < n; i++)
     {
-        int ele = lower_bound(a_copy.begin(), a_copy.end(), a[i]) - a_copy.begin();
-
-        pii it = seg.query(0, ele - 1);
-        seg.updatePoint(pos[i], {add(it.ff, 1), it.ss});
-
-        // cout << pos[i] << " " << it.ff + 1 << " " << it.ss << endl;
+        auto it = seg.query(0, pos[i] - 1);
+        seg.updatePoint(pos[i], {it.first + 1, it.second});
     }
-    pii ans = seg.query(0, n - 1);
-    cout << ans.ff << " " << ans.ss;
+
+    auto ans = seg.query(0, n);
+    cout << ans.first << " " << ans.second << endl;
 
     return 0;
 }
